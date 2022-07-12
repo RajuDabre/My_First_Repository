75047d3743a701085971c11dc2d8087c7d378f5f
Raju Dabre <Rajdabre03@gmail.com>
2022-07-12 22:03:39 +0530

    This is my second changes

--- src/practice/mondaypractice.java
+++ src/practice/mondaypractice.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class mondaypractice {
-
+    // This is  the changes
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\RajuChromedriver\\chromedriver.exe");
 		
